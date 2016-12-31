3fc85f3451b12a60f56fbd865c3f3d80ba638467
gadi2404 <gadi2404@gmail.com>
2016-12-31 18:53:00 +0200

    refactor component

--- build/components/tasks-list.js
+++ build/components/tasks-list.js
@@ -3,25 +3,25 @@ import {Link} from 'react-router';
 
 const TasksList = props => {
 		let that = props.that;
-
+		isActive = isActive.bind(that);
 		return <div>
 					<input id="checkAll" type="checkbox" hidden />
 					<label className="checkAllBtn" htmlFor="checkAll" onClick={that.handleCheckAll.bind(that)}></label>
 					<ul className="tasksList">
-						{ that.state.currentList.map( eachItem.bind(that) ) }
+						{ that.state.currentList.map( eachListItem.bind(that) ) }
 				   </ul>
 				   <div className="listStatus">
 				   		<span className="itemCount">{ that.state.listCount } tasks left</span>
 				   		<nav className="viewStatusBtns">
-				   			<Link to="/" className={isActive.call(that, '/')}>all</Link>
-				   			<Link to="/active" className={isActive.call(that, '/active')}>active</Link>
-				   			<Link to="/completed" className={isActive.call(that, '/completed')}>completed</Link>
+				   			<Link to="/" className={isActive()}>all</Link>
+				   			<Link to="/active" className={isActive('active')}>active</Link>
+				   			<Link to="/completed" className={isActive('completed')}>completed</Link>
 				   		</nav>
 				   </div>
 			   </div>
 }
 
-function eachItem(item, i){
+function eachListItem(item, i){
 		return <li key={i} className="listItem" data-index={i}>
 					<div className="checkboxWrap">
 						<input className="itemCheckbox" checked={this.state.currentList[i].status} type="checkbox" onChange={this.handleCheck.bind(this)} />
@@ -32,9 +32,10 @@ function eachItem(item, i){
 			   </li>
 }
 
-function isActive(btnName){
-	let currentRoute = this.props.location.pathname;
-	return btnName === currentRoute? 'active' : '';
+function isActive(btnName = ''){
+	let currentRoute = this.props.location.pathname,
+		prefix = '/';
+	return prefix + btnName === currentRoute? 'active' : '';
 }
 
 export default TasksList;
